3a9793a1eebc30a0f8c6d8442dda4bc6ecab3750
JPerfectSolution <j.perfectsolution@outlook.com>
2020-01-14 15:37:05 -0800

    update quote page

--- pages/quote.vue
+++ pages/quote.vue
@@ -40,33 +40,33 @@
             </div>
             <div class="card-content">
               <div class="content">
-                <div class="column is-12">
+                <div class="column is-12" v-for="(item, key) in collapse.species" :key="key">
                   <div class="columns">
                     <div class="column is-1">
                       <h6>Species</h6>
                     </div>
                     <div class="column is-1">
-                      <select>
-                        <option value="flint" selected>Flint</option>
-                        <option value="silver">Silver</option>
+                      <select v-model="item.species">
+                        <option value="" disabled>Select an Option</option>
+                        <option :value="spec" v-for="(spec, k1) in constSpecies" :key="k1">{{spec}}</option>
                       </select>
                     </div>
                     <div class="column is-1">
                       <h6>Dimension:</h6>
                     </div>
                     <div class="column is-1">
-                      <select>
-                        <option value="flint" selected>Flint</option>
-                        <option value="silver">Silver</option>
+                      <select v-model="item.dimension">
+                        <option value="0" disabled>Select an Option</option>
+                        <option :value="d.price" v-for="(d, k1) in dimensions" :key="k1">{{d.thick}}x{{d.width}}</option>
                       </select>
                     </div>
                     <div class="column is-1">
                       <h6>Length:</h6>
                     </div>
                     <div class="column is-1">
-                      <select>
-                        <option value="flint" selected>Flint</option>
-                        <option value="silver">Silver</option>
+                      <select v-model="item.length">
+                        <option value="" disabled>Select an Option</option>
+                        <option :value="spec" v-for="(spec, k1) in lengths" :key="k1">{{spec}}’</option>
                       </select>
                     </div>
                     <div class="column is-1">
@@ -74,11 +74,11 @@
                     </div>
                     <div class="column is-2">
                       <b-field>
-                        <b-numberinput v-model="number"></b-numberinput>
+                        <b-numberinput v-model="item.quantity"></b-numberinput>
                       </b-field>
                     </div>
                     <div class="column 1">
-                      <h6>Price</h6>
+                      <h6 v-if="item.dimension">${{item.dimension*item.quantity}}</h6>
                     </div>
                   </div>
                 </div>
@@ -103,15 +103,76 @@ export default {
       },
       number: 1,
       isOpen: 0,
+      constSpecies: ['Species 1', 'Species 2'],
+      dimensions: [
+        {thick:1, width:2, price: 1.20},
+        {thick:1, width:4, price: 1.20},
+        {thick:1, width:6, price: 1.00},
+        {thick:1, width:8, price: 1.00},
+        {thick:1, width:10, price: 1.10},
+        {thick:1, width:12, price: 1.10},
+        {thick:2, width:2, price: 1.00},
+        {thick:2, width:4, price: 0.80},
+        {thick:2, width:6, price: 0.80},
+        {thick:2, width:8, price: 0.80},
+        {thick:2, width:10, price: 0.90},
+        {thick:2, width:12, price: 0.90},
+        {thick:4, width:4, price: 0.80},
+        {thick:4, width:6, price: 0.80},
+        {thick:4, width:8, price: 0.80},
+        {thick:4, width:10, price: 1.10},
+        {thick:4, width:12, price: 1.10},
+        {thick:6, width:6, price: 0.80},
+        {thick:6, width:8, price: 1.00},
+        {thick:6, width:10, price: 1.20},
+        {thick:6, width:12, price: 1.10},
+        {thick:8, width:8, price: 1.00},
+        {thick:8, width:10, price: 1.30},
+        {thick:8, width:12, price: 1.20},
+      ],
+      lengths: [8, 10, 12, 14, 16, 18, 20, 22, 24],
       collapses: [{
           title: 'Rough Cut Full Dimensions',
+          species: [
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+          ],
+          additionalNote: ''
         },
         {
           title: 'Title 2',
+          species: [
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+          ],
+          additionalNote: '',
           text: 'Text 2'
         },
         {
           title: 'Title 3',
+          species: [
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+            {species: '', dimension: '', length: '', quantity: 0, price: 0},
+          ],
+          additionalNote: '',
           text: 'Text 3'
         }
       ]
